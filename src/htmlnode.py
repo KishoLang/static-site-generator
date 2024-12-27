c89b6c7e80e85a8cdcc8233cdb0337bd0b5f9197
KishoLang <aklangenberg@gmail.com>
2024-12-27 09:44:53 +0900

    add tests and node classes and conversion methods

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,9 +5,40 @@ class HTMLNode:
         self.children = children
         self.props = props
     
+    def __eq__(self, other):
+        return isinstance(other, HTMLNode) and self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props
+
     def to_html(self):
         raise NotImplementedError()
 
     def props_to_html(self):
         return "".join(map(lambda key: f' {key}="{self.props[key]}"', self.props))
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = {}):
+        super().__init__(tag = tag, value = value, props = props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError()
+        if self.tag == None:
+            return f"{str(self.value)}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag = tag, children = children, props = props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No tag")
+        if self.children == None:
+            raise ValueError("No value")
+        result = ""
+        for child in self.children:
+            result = result + child.to_html()
+        return f"<{self.tag}>{result}</{self.tag}>"  
     
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -30,8 +30,105 @@ class TestHTMLNode(unittest.TestCase):
         html_prop_answer =  ' href="https://www.google.com" target="_blank"'
         self.assertEqual(html_prop, html_prop_answer)
 
-    
+    def test_repr(self):
+        html_node = HTMLNode("p", "this is a test paragraph", children=None, props={"style": "font-size: 16px; color: black;"})
+        repr_html_node = "HTMLNode(p, this is a test paragraph, None, {'style': 'font-size: 16px; color: black;'})"
+        repr_html_test_node = html_node.__repr__()
+        self.assertEqual(repr_html_test_node, repr_html_node)
 
+    def test_eq(self):
+        html_node1 = HTMLNode("p", "this is another test", children=None, props={"id": "parag"})
+        html_node2 = HTMLNode("p", "this is another test", children=None, props={"id": "parag"})
+        self.assertEqual(html_node1, html_node2)
+    
+    def test_eq2(self):
+        html_node1 = HTMLNode("a", "this is another test", children=None, props={"id": "parag"})
+        html_node2 = HTMLNode("p", "this is another test", children=None, props={"id": "parag"})
+        self.assertNotEqual(html_node1, html_node2)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        leaf_node1 = LeafNode("p", "This is a paragraph of text.")
+        answer = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(answer, leaf_node1.to_html())
+    
+    def test_to_html2(self):
+        leaf_node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        answer = """<a href="https://www.google.com">Click me!</a>"""
+        self.assertEqual(answer, leaf_node2.to_html())
+
+class TestParentNode(unittest.TestCase):
+    def test_basic_case(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        test_case = node.to_html()
+        expected_case = """<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>""" 
+        self.assertEqual(test_case, expected_case)
+    
+    def test_nested_case(self):
+        nested_node = ParentNode("div", [
+            LeafNode("p", "First paragraph"),
+            ParentNode("section", [
+                LeafNode("h1", "Title"),
+                ParentNode("article", [
+                    LeafNode("p", "Article text"),
+                    LeafNode("i", "Italic text")
+                ]),
+                LeafNode("footer", "Footer text")
+            ]),
+            LeafNode("p", "Last paragraph")
+        ])
+        test_case = nested_node.to_html()
+        expected_case = """<div><p>First paragraph</p><section><h1>Title</h1><article><p>Article text</p><i>Italic text</i></article><footer>Footer text</footer></section><p>Last paragraph</p></div>"""
+        self.assertEqual(test_case, expected_case)
+
+    def test_no_children(self):
+        node = ParentNode("div", [])
+        test_case = node.to_html()
+        expected_case = """<div></div>""" 
+        self.assertEqual(test_case, expected_case)
+    
+    def test_no_tag(self):
+        node = ParentNode(None, [LeafNode("p", "text")])
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_none_as_children(self):
+        node = ParentNode("div", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_deep_nested(self):
+        node = ParentNode("div", [
+            LeafNode(None, "plain text"),
+            ParentNode("p", [
+                LeafNode(None, "more text"),
+                LeafNode("span", "spantext")
+            ])
+        ])
+        test_case = node.to_html()
+        expected_case = """<div>plain text<p>more text<span>spantext</span></p></div>"""
+        self.assertEqual(test_case, expected_case)
+
+    def test_multiple_parent_nodes(self):
+        node = ParentNode("main", [
+            ParentNode("nav", [
+                LeafNode("a", "Link")
+            ]),
+            ParentNode("div", [
+                LeafNode("p", "Para")
+            ])
+        ])
+        test_case = node.to_html()
+        expected_case = """<main><nav><a>Link</a></nav><div><p>Para</p></div></main>"""
+        self.assertEqual(test_case, expected_case)
     
 
 
