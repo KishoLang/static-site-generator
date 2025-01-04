b31d3c2d7061f1276855d2426f3f7a5595e8ae54
KishoLang <aklangenberg@gmail.com>
2025-01-04 16:32:56 +0900

    add image and link node splitter

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 from textnode import TextNode, TextType, text_node_to_html_node
 from extract_markdown_links import extract_markdown_images, extract_markdown_links
+from text_node_splitter import split_nodes_image, split_nodes_link, split_nodes_delimiter
 
 def main():
-    images_text = "This is text with a and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(images_text))
+    link_text = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    # print(extract_markdown_links(link_text))
 
-    links_text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(links_text))
-    
-    images_text2 = ""
-    print(extract_markdown_images(images_text2))
+    # # node1 = TextNode(
+    # # "This is text with an image ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",TextType.TEXT)
+    # # new_nodes = split_nodes_image([node1])
+    # # print(new_nodes)
+
+
+
+    # node2 = TextNode(
+    # "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",TextType.TEXT)
+    # new_link_nodes = split_nodes_link([node2])
+    # print(new_link_nodes)
+
 
     
 
--- src/text_node_splitter.py
+++ src/text_node_splitter.py
@@ -1,5 +1,6 @@
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode
+from extract_markdown_links import extract_markdown_links, extract_markdown_images
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -17,7 +18,42 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(TextNode(split_text, text_type))
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        if node.text_type == TextType.IMAGE:
+            new_nodes.append(node)
+        else:
+            extracted_images = extract_markdown_images(current_text)
+            for image in extracted_images:
+                parts = current_text.split(f"![{image[0]}]({image[1]})", 1)
+                if parts[0] != "":
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+                current_text = parts[1]
+            if current_text != "":
+                new_nodes.append(TextNode(current_text, TextType.TEXT))
+    return new_nodes
 
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        if node.text_type == TextType.LINK:
+            new_nodes.append(node)
+        else:
+            extracted_links = extract_markdown_links(current_text)
+            for link in extracted_links:
+                parts = current_text.split(f"[{link[0]}]({link[1]})", 1)
+                if parts[0] != "":
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+                if len(parts) > 1:
+                    current_text = parts[1]
+            if current_text != "":
+                new_nodes.append(TextNode(current_text, TextType.TEXT))
+    return new_nodes
 
 
             
